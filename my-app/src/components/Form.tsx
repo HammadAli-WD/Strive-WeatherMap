ebd871ef8738fc3c0b7d46c88fdaf631c991c4a2
Hammad-87 <pakswim@gmail.com>
2020-08-07 09:42:10 +0200

    Data Entry Form

---
+++ my-app/src/components/Form.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+interface IFormComponentProps {
+    getWeather: (e: any) => Promise<any>
+}
+
+interface IFormComponentStates {
+}
+
+class Form extends React.Component<IFormComponentProps, IFormComponentStates> {
+    constructor(props: IFormComponentProps) {
+        super (props);
+    }
+    
+    render() {
+        return (
+            <form onSubmit={this.props.getWeather}>
+                <input type="text" name="city" placeholder="City.."/>
+                <input type="text" name="country" placeholder="Country.."/>
+                <button>Get Weather</button>
+            </form>
+        );
+    }
+}
+
+export default Form;
