aa66cb31e39d99195097236998a268ad678f9721
Hammad-87 <pakswim@gmail.com>
2020-08-07 16:04:18 +0200

    updated

--- my-app/src/components/Titles.tsx
+++ my-app/src/components/Titles.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-
+//import {Jumbotron, Container} from "react-bootstrap"
 
 class Titles extends React.Component {
     render() {
@@ -8,6 +8,14 @@ class Titles extends React.Component {
                 <h1 className="primary" >Weather Finder</h1>
                 <p>Find out temperature, conditions, and more...</p>
             </div>
+           /*  <Jumbotron fluid>
+            <Container>
+                <h1>Weather Map</h1>
+                <p>
+               Here you can find the daily temperature, humidity and other weather conditions
+                </p>
+            </Container>
+            </Jumbotron> */
         );
     }
 };
--- my-app/src/components/Weather.tsx
+++ my-app/src/components/Weather.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import './Weather.css';
 
 interface IWeatherProps {
     temperature: number | undefined,
@@ -17,18 +18,50 @@ class Weather extends React.Component<IWeatherProps, IWeatherStates> {
     constructor(props: IWeatherProps) {
         super(props);
     }
-
     render() {
         return (
-            <div>
-                {this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
-                {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
-                {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
-                {this.props.description && <p>Conditions: {this.props.description}</p>}
-                {this.props.error && <p>Error: {this.props.error} for city: {this.props.city} and country: {this.props.country}</p>}
+
+<div className="page-content page-container" id="page-content">
+    <div className="padding">
+        <div className="row container d-flex justify-content-center">
+            <div className="col-lg-8 grid-margin stretch-card">
+              
+                <div className="card card-weather">
+                    <div className="card-body">
+                        <div className="weather-date-location">
+                            
+                            <p className="text-gray"> <span className="weather-location">{this.props.city}, {this.props.country}</span> </p>
+                        </div>                        
+                        <div className="weather-data d-flex">
+                            <div className="mr-auto">
+                                <h4 className="display-3">{this.props.temperature} <span className="symbol"> °</span>C</h4>
+                                <p> {this.props.description} </p>
+                                <p>Humidity {this.props.humidity} </p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+                
             </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+          
         );
     }
 };
 
 export default Weather;
+
+  {/* <div>
+                {this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
+                {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
+                {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+                {this.props.description && <p>Conditions: {this.props.description}</p>}
+                {this.props.error && <p>Error: {this.props.error} for city: {this.props.city} and country: {this.props.country}</p>}
+            </div> */}
